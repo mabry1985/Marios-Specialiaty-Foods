9b88b52b879e10d4a2b4b40afbd1391123af629f
Josh Mabry <mabry1985@gmail.com>
2019-08-09 08:59:13 -0700

    add table schema and create mvc files and tables for products and reviews

---
+++ db/migrate/20190809155613_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :author
+      t.integer :rating
+      t.text :content_body
+      t.belongs_to :products, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
