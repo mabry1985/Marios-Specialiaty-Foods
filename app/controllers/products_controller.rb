dcd4b590d980fdd078e56a58a21604c658a2981b
Josh Mabry <mabry1985@gmail.com>
2019-08-09 10:28:01 -0700

    add show view and method to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,10 +14,25 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = "Product added to database"
-      redirect_to_products_path
+      redirect_to products_path
     else
       render :new
     end
   end
-  
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
+
 end
