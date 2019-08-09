a4ecef941f068a5b69aab800f1ad394046e07a24
Josh Mabry <mabry1985@gmail.com>
2019-08-09 09:42:23 -0700

    add new and create methods to product controller and add new view to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product added to database"
+      redirect_to_products_path
+    else
+      render :new
+    end
+  end
+  
 end
