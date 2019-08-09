68c3af7bd8d758e1874d9f3af12fc5061bc42a65
Josh Mabry <mabry1985@gmail.com>
2019-08-09 14:12:58 -0700

    spin wheels on show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ProductsController < ApplicationController
 
   def index
@@ -22,6 +24,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    binding.pry
     render :show
   end
 
