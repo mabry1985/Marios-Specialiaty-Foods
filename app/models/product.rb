fc7bca72982986e6b30b78528bb3f2e31e06335b
Josh Mabry <mabry1985@gmail.com>
2019-08-10 17:22:32 -0700

    add callbacks and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  before_save(:titleize_product)
+  validates :name, presence: true
+  validates :country_of_origin, presence: true
+  validates :cost, presence: true
+
+  private
+  def titleize_product
+    self.name = self.name.titleize
+  end
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :author, presence: true
+  validates :content_body, presence: true, length: { in: 50..250 }
+  validates :rating, presence: true, length: { in: 1..5 }
 end
